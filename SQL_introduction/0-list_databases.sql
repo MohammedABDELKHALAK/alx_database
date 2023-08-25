5eb63b9d24842121264fd94d204e2901a1f5dbeb
unknown <faculty199568@gmail.com>
2023-08-25 15:53:27 +0100

    task done

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,11 +1,5 @@
--- Create 4 databases
-CREATE DATABASE IF NOT EXISTS my_db_01;
-CREATE DATABASE IF NOT EXISTS my_db_02;
-CREATE DATABASE IF NOT EXISTS my_db_03;
-CREATE DATABASE IF NOT EXISTS holbteron_db;
+
 --  lists all databases of your MySQL server.
-SELECT 'Database'
-UNION
 SELECT schema_name
 FROM information_schema.schemata
 WHERE schema_name NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys');
