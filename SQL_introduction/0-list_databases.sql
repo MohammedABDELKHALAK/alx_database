9def14696fd81a022eceae6b416b8c2860a13b82
unknown <faculty199568@gmail.com>
2023-08-25 15:31:39 +0100

    task done

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -2,4 +2,5 @@
 
 --  lists all databases of your MySQL server.
 SELECT schema_name
-FROM information_schema.schemata;
+FROM information_schema.schemata
+WHERE schema_name NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys');
