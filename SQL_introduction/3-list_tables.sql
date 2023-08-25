cfaa29c3bd76c0f4c3531743fde3937eceaf1068
unknown <faculty199568@gmail.com>
2023-08-25 16:26:50 +0100

    task done

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,7 @@
 
 -- lists all the tables of a database in your MySQL server.
 USE holbteron_db;
+CREATE holbteron_0 IF NOT EXISTS
+CREATE holbteron_1 IF NOT EXISTS
+CREATE holbteron_2 IF NOT EXISTS
 SHOW TABLES;
