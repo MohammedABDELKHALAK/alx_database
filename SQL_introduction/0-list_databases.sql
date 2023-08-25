ca92ae8a47b5edd4103be2b2a297965f06734209
unknown <faculty199568@gmail.com>
2023-08-25 15:34:44 +0100

    task done

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,8 @@
-
-
+-- Create 4 databases
+CREATE DATABASE IF NOT EXISTS my_db_01;
+CREATE DATABASE IF NOT EXISTS my_db_02;
+CREATE DATABASE IF NOT EXISTS my_db_03;
+CREATE DATABASE IF NOT EXISTS holbteron_db;
 --  lists all databases of your MySQL server.
 SELECT 'Database'
 UNION
