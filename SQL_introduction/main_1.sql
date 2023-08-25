35c5cbda5ed526cf4700827a6406f0f65d9b098b
unknown <faculty199568@gmail.com>
2023-08-25 16:21:13 +0100

    task done

--- SQL_introduction/main_1.sql
+++ SQL_introduction/main_1.sql
@@ -3,3 +3,7 @@ CREATE DATABASE IF NOT EXISTS my_db_01;
 CREATE DATABASE IF NOT EXISTS my_db_02;
 CREATE DATABASE IF NOT EXISTS my_db_03;
 CREATE DATABASE IF NOT EXISTS holbteron_db;
+
+-- Delete and create database hbtn_test_db_0
+DROP DATABASE IF EXISTS hbtn_test_db_0;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
