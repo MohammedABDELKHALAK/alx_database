dc5a3b33280a59c9eb957a82470e7aead53884af
unknown <faculty199568@gmail.com>
2023-08-25 17:02:17 +0100

    task done

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -4,4 +4,4 @@ SELECT 'Database'
 UNION
 SELECT schema_name
 FROM information_schema.schemata
-WHERE schema_name NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys');
+WHERE schema_name IN ('information_schema', 'mysql', 'performance_schema', 'sys');
