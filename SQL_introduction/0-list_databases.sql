6a7cd6545cee02d87459a5aa0558c02a4ecb6bb6
unknown <faculty199568@gmail.com>
2023-08-25 17:01:02 +0100

    task done

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,7 @@
 
 --  lists all databases of your MySQL server.
+SELECT 'Database'
+UNION
 SELECT schema_name
 FROM information_schema.schemata
-WHERE schema_name IN ('holbteron_db', 'information_schema', 'mysql', 'performance_schema', 'sys');
+WHERE schema_name NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys');
