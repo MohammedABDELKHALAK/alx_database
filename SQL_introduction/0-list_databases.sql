519b6409221588b4ef2256108c09979e0639e359
unknown <faculty199568@gmail.com>
2023-08-25 17:08:53 +0100

    task done

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,7 +1,6 @@
 
 --  lists all databases of your MySQL server.
-SELECT 'Database'
-UNION
-SELECT schema_name
-FROM information_schema.schemata
-WHERE schema_name IN ('information_schema', 'mysql', 'performance_schema', 'sys');
+SELECT 'Database' UNION
+SELECT schema_name FROM information_schema.schemata
+WHERE schema_name NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys')
+ORDER BY schema_name;
