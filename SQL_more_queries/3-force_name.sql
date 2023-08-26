d7c38cd674b42036968cbc3cfc2812a6bf86d422
unknown <faculty199568@gmail.com>
2023-08-26 21:38:16 +0100

    create new folder sql more queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,4 @@
-
-
 --Write a script that creates the table force_name on your MySQL server.
-
 CREATE TABLE IF NOT EXISTS hbtn_test_db_3.force_name (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
