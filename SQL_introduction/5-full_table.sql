27ffd437eafe3747c121dadf44401262b6680c5c
unknown <faculty199568@gmail.com>
2023-08-26 16:18:42 +0100

    task done

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,18 @@
 
+
+-- Create database and table
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+USE hbtn_test_db_5;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
+
+
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 USE hbtn_test_db_5;
 
