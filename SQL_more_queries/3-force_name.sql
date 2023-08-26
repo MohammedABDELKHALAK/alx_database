ade5d2fbcd7df2234df1bad67205230d78199114
unknown <faculty199568@gmail.com>
2023-08-26 21:46:54 +0100

    create new folder sql more queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,13 +1,22 @@
 
 
---Write a script that creates the table force_name on your MySQL server.
+-- Write a script that creates the table force_name on your MySQL server.
+-- New database hbtn_test_db_3
+DROP DATABASE IF EXISTS hbtn_test_db_3;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_3;
 
 USE hbtn_test_db_3;
 
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    id INT,
+    name VARCHAR(256) NOT NULL
 );
 
+
+-- Regular inserts + select
+INSERT INTO force_name (id, name) VALUES (1, "Holberton School");
+INSERT INTO force_name (id, name) VALUES (2, "Holberton");
+INSERT INTO force_name (id, name) VALUES (3, "School");
+INSERT INTO force_name (id, name) VALUES (4, "C is fun");
+INSERT INTO force_name (id, name) VALUES (1, "Python is cool");
 
