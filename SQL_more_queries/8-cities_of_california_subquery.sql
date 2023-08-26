2478b7afb6920733f1a8e02b24bf2eccd356b8d0
unknown <faculty199568@gmail.com>
2023-08-26 20:19:05 +0100

    create new folder sql more queries

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,9 @@
 
 
 -- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+
+DROP DATABASE IF EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 SELECT id, name
