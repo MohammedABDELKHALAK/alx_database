c9de08e620e73ed4dc74991ae68e7ff57dc87b7c
unknown <faculty199568@gmail.com>
2023-08-26 16:03:55 +0100

    task done

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 
 --  displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
-USE hbtn_0c_0;
+USE hbtn_test_db_8;
 SELECT COUNT(*) AS record_count FROM first_table WHERE id = 89;
--- SQL_introduction/main_0.sql
+++ SQL_introduction/main_0.sql
@@ -59,3 +59,22 @@ CREATE TABLE IF NOT EXISTS first_table (
 INSERT INTO first_table (id, name) VALUES (1, "First name");
 INSERT INTO first_table (id, name) VALUES (2, "Last name");
 INSERT INTO first_table (id, name) VALUES (3, "name");
+
+-- Create database 
+DROP DATABASE IF EXISTS hbtn_test_db_8;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
+USE hbtn_test_db_8;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
+-- Create records
+INSERT INTO first_table (id, name) VALUES (1, "A");
+INSERT INTO first_table (id, name) VALUES (89, "B");
+INSERT INTO first_table (id, name) VALUES (2, "C");
+INSERT INTO first_table (id, name) VALUES (89, "D");
+INSERT INTO first_table (id, name) VALUES (89, "E");
+INSERT INTO first_table (id, name) VALUES (3, "F");
