1e4110addffe93296221a979e4cacf207d66199c
unknown <faculty199568@gmail.com>
2023-08-26 16:00:35 +0100

    task done

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -2,5 +2,5 @@
 --  inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
 
 
-USE hbtn_0c_0;
+USE hbtn_test_db_7;
 INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
