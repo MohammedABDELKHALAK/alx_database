f97744e2f9e07f8143accc8bb27c4b171c39acfb
unknown <faculty199568@gmail.com>
2023-08-26 15:08:58 +0100

    task done

--- SQL_introduction/main_0.sql
+++ SQL_introduction/main_0.sql
@@ -33,3 +33,19 @@ CREATE TABLE IF NOT EXISTS first_table (
     created_at DATE,
     PRIMARY KEY (id)
 );
+
+-- Create database 
+DROP DATABASE IF EXISTS hbtn_test_db_6;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_6;
+USE hbtn_test_db_6;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
+-- Insert 3 records
+INSERT INTO first_table (id, name) VALUES (1, "First name");
+INSERT INTO first_table (id, name) VALUES (2, "Last name");
+INSERT INTO first_table (id, name) VALUES (3, "name");
