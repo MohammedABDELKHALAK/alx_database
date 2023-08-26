8a6c929f1c48c5b6387ca49a214f7d4514440e61
unknown <faculty199568@gmail.com>
2023-08-26 16:26:07 +0100

    task done

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,23 +1,13 @@
+-- Create database and table
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
 
-
-
--- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 USE hbtn_test_db_5;
 
-
-SELECT CONCAT('CREATE TABLE `', first_table, '` (',
-    GROUP_CONCAT(
-        '`', column_name, '`', ' ', column_type,
-        IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
-        IF(column_default IS NOT NULL, CONCAT(' DEFAULT ', column_default), ''),
-        IF(column_key = 'PRI', ' PRIMARY KEY', '')
-        SEPARATOR ', '
-    ),
-    ') ENGINE=', engine,
-    IF(table_collation IS NOT NULL, CONCAT(' COLLATE ', table_collation), ''),
-    ';') AS create_statement
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
-
-
-
+CREATE TABLE `first_table` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(128) DEFAULT NULL,
+    `c` CHAR(1) DEFAULT NULL,
+    `created_at` DATE DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
