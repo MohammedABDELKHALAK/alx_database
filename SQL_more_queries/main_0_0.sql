52bca6588d65b5f6ec770884a207f398934c8ee4
unknown <faculty199568@gmail.com>
2023-08-26 21:50:18 +0100

    create new folder sql more queries

--- SQL_more_queries/main_0_0.sql
+++ SQL_more_queries/main_0_0.sql
@@ -1,18 +1,5 @@
 
 
--- New database hbtn_test_db_3
-DROP DATABASE IF EXISTS hbtn_test_db_3;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_3;
-USE hbtn_test_db_3;
--- Regular inserts + select
-INSERT INTO force_name (id, name) VALUES (1, "Holberton School");
-INSERT INTO force_name (id, name) VALUES (1, "Python is cool");
-INSERT INTO force_name (id, name) VALUES (2, "Holberton");
-INSERT INTO force_name (id, name) VALUES (3, "School");
-INSERT INTO force_name (id, name) VALUES (4, "C is fun");
-
-SELECT * FROM force_name ORDER BY id, name ASC;
-
 -- New database hbtn_test_db_4
 DROP DATABASE IF EXISTS hbtn_test_db_4;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
