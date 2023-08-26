275e4b14789bb54913b6e29ad8fbe6fc7b9618db
unknown <faculty199568@gmail.com>
2023-08-26 14:55:47 +0100

    task done

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,6 +2,6 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_test_db_5' AND TABLE_NAME = 'first_table';
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
 
 
--- SQL_introduction/main_0.sql
+++ SQL_introduction/main_0.sql
@@ -1,6 +1,7 @@
 -- Delete and create database hbtn_test_db_0 and create 3 tables
 DROP DATABASE IF EXISTS hbtn_test_db_0;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+
 USE hbtn_test_db_0;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (
@@ -19,11 +20,16 @@ CREATE TABLE IF NOT EXISTS holbteron_2 (
 -- Create database and table
 DROP DATABASE IF EXISTS hbtn_test_db_5;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
-USE hbtn_test_db_5;
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+USE hbtn_0c_0;
+
 CREATE TABLE IF NOT EXISTS first_table (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128),
-    c CHAR(1),
-    created_at DATE,
+    name VARCHAR(128) DEFAULT NULL ,
+    c CHAR(1) DEFAULT NULL ,
+    created_at DATE DEFAULT NULL ,
     PRIMARY KEY (id)
 );
