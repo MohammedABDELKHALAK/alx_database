2da4c43ff36cfa88394e30ede8faed869bc0a684
unknown <faculty199568@gmail.com>
2023-08-26 21:02:11 +0100

    create new folder sql more queries

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -3,7 +3,7 @@
 --Write a script that creates the table id_not_null on your MySQL server.
 
 
-USE hbtn_test_db_4;
+USE hbtn_0d_2;
 
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT NOT NULL DEFAULT 1,
