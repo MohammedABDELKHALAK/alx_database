ccfe4960bcf7d9883bab10363329c57ab84fd473
unknown <faculty199568@gmail.com>
2023-08-26 15:52:26 +0100

    task done

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,7 @@
 
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+USE hbtn_test_db_5;
+
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
