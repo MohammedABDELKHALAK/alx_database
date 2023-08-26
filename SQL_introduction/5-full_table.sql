6e4f6b6eae69eb82c6d690b3ab6e89020d3d3bcf
unknown <faculty199568@gmail.com>
2023-08-26 16:30:50 +0100

    task done

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -5,6 +5,9 @@
 USE hbtn_test_db_5;
 
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_test_db_5' AND TABLE_NAME = 'first_table';
+SET @table_name = 'first_table';
+SET @sql_query = CONCAT('SHOW CREATE TABLE ', @table_name);
+
+PREPARE stmt FROM @sql_query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
