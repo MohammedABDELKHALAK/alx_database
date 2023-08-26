545f30ac49a1b525ba6883cfba591471a6a3378a
unknown <faculty199568@gmail.com>
2023-08-26 21:28:18 +0100

    create new folder sql more queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -14,10 +14,3 @@ CREATE TABLE IF NOT EXISTS force_name (
     PRIMARY KEY (id)
 );
 
-
--- Regular inserts + select
-INSERT INTO hbtn_test_db_3.force_name (id, name) VALUES (1, "Holberton School");
-INSERT INTO hbtn_test_db_3.force_name (id, name) VALUES (1, "Python is cool");
-INSERT INTO hbtn_test_db_3.force_name (id, name) VALUES (2, "Holberton");
-INSERT INTO hbtn_test_db_3.force_name (id, name) VALUES (3, "School");
-INSERT INTO hbtn_test_db_3.force_name (id, name) VALUES (4, "C is fun");
