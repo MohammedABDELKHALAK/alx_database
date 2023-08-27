7388d8cc4a25d6a49ea6931d7c81614ad91f6b3f
unknown <faculty199568@gmail.com>
2023-08-27 02:31:32 +0100

    create new folder sql more queries

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,7 +3,7 @@
 
 -- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 
-USE btn_test_db_8;
+USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
