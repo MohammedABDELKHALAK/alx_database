342837ad5d16734cb18cc147f090de90a41ef44f
unknown <faculty199568@gmail.com>
2023-08-27 02:30:37 +0100

    create new folder sql more queries

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 
 -- Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 
-USE btn_test_db_3;
+USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT,
