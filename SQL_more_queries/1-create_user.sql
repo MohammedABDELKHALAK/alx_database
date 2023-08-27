d7a77efc92461322f931d99ca46f7f9ebeae6203
unknown <faculty199568@gmail.com>
2023-08-27 02:00:21 +0100

    create new folder sql more queries

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 
 -- Write a script that creates the MySQL server user user_0d_1.
 
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'user_0d_1_pwd';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+DROP USER 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
