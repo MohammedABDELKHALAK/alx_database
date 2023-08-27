7358c66e4d822f99177b1c0018c3d4ad0eea2c7f
unknown <faculty199568@gmail.com>
2023-08-27 02:27:44 +0100

    create new folder sql more queries

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -4,7 +4,7 @@
 DROP DATABASE IF EXISTS hbtn_test_db_4;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
 USE hbtn_test_db_4;
-CREATE TABLE IF NOT EXISTS 'id_not_null' (
+CREATE TABLE IF NOT EXISTS hbtn_test_db_4.id_not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
 );
